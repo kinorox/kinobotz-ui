29d73cccfd058a3b66b0f9eacf54716eab54f713
kinorox <gabriel.bombardi@gmail.com>
2023-07-05 18:45:34 -0300

    cleanup on navbar and index

--- src/App.vue
+++ src/App.vue
@@ -10,7 +10,7 @@
 
 <template>
     <div class="container">
-        <!--<nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
             <div class="container-fluid">
                 <a class="navbar-brand" href="#">Navbar</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -20,12 +20,13 @@
                     <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                         <li class="nav-item">
                             <router-link class="nav-link active" aria-current="page" to="/">Home</router-link>
-                            <router-link class="nav-link active" aria-current="page" to="/overlay/12345">Overlay</router-link>
+                        </li>
+                        <li class="nav-item">
+                            <router-link class="nav-link active" aria-current="page" to="/">Overlay</router-link>
                         </li>
                     </ul>
                 </div>
             </div>
-        </nav>-->
-        <router-view></router-view>
+        </nav>
     </div>
 </template>

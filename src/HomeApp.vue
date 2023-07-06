6f988a35e2e609b8796daf935dab88952d4768a9
kinorox <gabriel.bombardi@gmail.com>
2023-07-06 00:19:22 -0300

    components separation

---
+++ src/HomeApp.vue
@@ -0,0 +1,12 @@
+<script>
+import NavBar from './components/NavBar.vue';
+
+export default {
+    name: "HomeApp",
+    components: { NavBar }
+}
+</script>
+
+<template>
+    <NavBar/>
+</template>
---
+++ src/components/NavBar.vue
@@ -0,0 +1,26 @@
+<script>
+    export default {
+        name: 'NavBar'
+    }
+</script>
+
+<template>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <router-link class="nav-link active" aria-current="page" to="/">Home</router-link>
+                    </li>
+                    <li class="nav-item">
+                        <router-link class="nav-link active" aria-current="page" to="/overlay/55b7c1e0-1800-436c-a500-7094e8981257">Overlay</router-link>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>

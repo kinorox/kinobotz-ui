87657227b5e04fa0e13a89e65cb50fa2d571454f
kinorox <gabriel.bombardi@gmail.com>
2023-08-14 17:34:55 -0300

    fa icons on homepage

--- src/HomeApp.vue
+++ src/HomeApp.vue
@@ -19,7 +19,16 @@ export default {
 </style>
 
 <template>
-    <div class="container-home">
+    <div class="container-home flex-column">
         <NavBar/>
+        <ul class="list-group list-group-horizontal">
+            <li class="list-group-item"><a href="https://twitch.tv/k1notv" target=”_blank”><font-awesome-icon :icon="['fab', 'twitch']" class="icon alt"/></a></li>
+            <li class="list-group-item"><a href="https://twitter.com/k1notv" target=”_blank”><font-awesome-icon :icon="['fab', 'twitter']" class="icon alt"/></a></li>
+            <li class="list-group-item"><a href="https://github.com/kinorox" target=”_blank”><font-awesome-icon :icon="['fab', 'github']" class="icon alt"/></a></li>
+            <li class="list-group-item"><a href="https://www.instagram.com/k1notv/" target=”_blank”><font-awesome-icon :icon="['fab', 'instagram']" class="icon alt"/></a></li>
+            <li class="list-group-item"><a href="https://discord.gg/YdwQutt" target=”_blank”><font-awesome-icon :icon="['fab', 'discord']" class="icon alt"/></a></li>
+        </ul>
+        <br>
+        contatok1notv@gmail.com
     </div>
 </template>
--- src/main.js
+++ src/main.js
@@ -5,13 +5,14 @@ import { HubConnectionBuilder } from '@microsoft/signalr';
 import router from '@/scripts/router'
 import store from '@/scripts/store'
 import 'bootstrap/dist/css/bootstrap.css';
-import { library } from '@fortawesome/fontawesome-svg-core'
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
-import { faUserSecret } from '@fortawesome/free-solid-svg-icons'
 import Vue3EasyDataTable from 'vue3-easy-data-table';
 import 'vue3-easy-data-table/dist/style.css';
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+import { fab } from "@fortawesome/free-brands-svg-icons";
 
-library.add(faUserSecret)
+// add the icon style(s) you have installed to the library
+library.add(fab);
 
 var url = 'https://kinobotz.herokuapp.com';
 if(process.env.NODE_ENV === 'development') {
@@ -24,8 +25,8 @@ const connection = new HubConnectionBuilder()
 
 createApp(App)
     .component('EasyDataTable', Vue3EasyDataTable)
+    .component("font-awesome-icon", FontAwesomeIcon)
     .use(router)
     .use(store)
-    .use('font-awesome-icon', FontAwesomeIcon)
     .use(VueSignalR, { connection })
     .mount('#app');
